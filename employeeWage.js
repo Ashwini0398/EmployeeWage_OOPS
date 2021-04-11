32da5dfcf44e03272261a41e7e3847feef98ac26
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-04-11 13:01:09 +0530

    MODIFIED: calculate daily emp wages

--- employeeWage.js
+++ employeeWage.js
@@ -1,5 +1,7 @@
 console.log("Welcome to employee wage problem");
 
+let IS_FULL_TIME = 1;
+let Emp_Rate_Per_Hr = 20;
 let IS_ABSENT = 0;
 let empCheck = Math.floor(Math.random() * 10 ) % 2;
 
@@ -17,7 +19,20 @@ class Employee{
              }
             return empCheck;
         }
+    
+        dailyWage(){
+            let empHrs = 0;
+            let empWage = 0;
+            if(empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else{
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hr;
+            console.log(`Daily emplyee wages are : ${empWage}`);
+        }
 }
 
-let result = new Employee().attendance();
-console.log(result);
+let result = new Employee().dailyWage();
