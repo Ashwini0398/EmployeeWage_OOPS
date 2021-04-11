2e9d3b0ac33f542006d49e08fef73183adbc1d13
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-04-11 12:49:36 +0530

    ADDED: check wheather employee present or absent

--- employeeWage.js
+++ employeeWage.js
@@ -1,1 +1,23 @@
 console.log("Welcome to employee wage problem");
+
+let IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10 ) % 2;
+
+class Employee{
+
+    constructor(){}
+     attendance(){
+            if(empCheck == IS_ABSENT)
+            {
+                console.log("UC1-Employee is ABSENT");
+    
+            }
+            else{
+                console.log("UC1-Employee is PRESENT");
+             }
+            return empCheck;
+        }
+}
+
+let result = new Employee().attendance();
+console.log(result);
