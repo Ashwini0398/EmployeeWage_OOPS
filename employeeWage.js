129086d04a001cacef4b8c7b55594b29bc9ab540
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-04-11 13:23:41 +0530

    MODIFIED: calculate emplyee working time using switch case

--- employeeWage.js
+++ employeeWage.js
@@ -2,9 +2,12 @@ console.log("Welcome to employee wage problem");
 
 let IS_FULL_TIME = 1;
 let IS_PART_TIME = 2;
+
 let Emp_Rate_Per_Hr = 20;
 let IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10 ) % 2;
+let empHrs = 0;
+let empWage = 0;
+let empCheck = Math.floor(Math.random() * 10 ) % 3;
 
 class Employee{
 
@@ -21,23 +24,29 @@ class Employee{
             return empCheck;
         }
     
-        calculateWage(){
-            let empHrs = 0;
-            let empWage = 0;
-            if(empCheck == IS_PART_TIME)
-            {
+        dailywages(){
+            switch(empCheck){
+            case IS_FULL_TIME:
                 empHrs = 8;
-            }
-            else if (empCheck == IS_FULL_TIME) {
+                break;
+
+            case IS_PART_TIME:
                 empHrs = 4;
-                
-            } else {
-                
+                break;
+
+            default:
                 empHrs = 0;
-            }
+                break;
+            } 
+            return empCheck;
+        }
+
+        calculateWage(){
             empWage = empHrs * Emp_Rate_Per_Hr;
-            console.log(`Employee wages are : ${empWage}`);
+            return empWage;
         }
 }
 
+let employee = new Employee().dailywages();
 let result = new Employee().calculateWage();
+console.log(`Employee Wages are ${result}`);
